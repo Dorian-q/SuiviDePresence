7b8dfe47ad4dd12c2d491e616852f02fefdcad5e
Dorian-q <dorian.quignon@isen.yncrea.fr>
2022-02-09 16:32:28 +0100

    QRCode

--- app/src/main/java/com/example/suividepresence/MainActivity.kt
+++ app/src/main/java/com/example/suividepresence/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.suividepresence
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var editText: EditText
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initializing views
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.editText)
+        imageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            if (text.isNotBlank()) {
+                val bitmap = generateQRCode(text)
+                imageView.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    fun generateQRCode(content: String): Bitmap {
+        val writer = QRCodeWriter()
+        val size = 512
+        val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, size, size)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+        return bitmap
     }
 }
